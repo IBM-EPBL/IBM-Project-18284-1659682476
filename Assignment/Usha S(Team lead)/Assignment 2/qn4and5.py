a7f50fc5c8bb6c87e318415ade99d5ee95290e9b
unknown <mayilsamymv@gmail.com>
2022-09-18 22:11:52 +0530

    Literature survey

---
+++ Assignment/Usha S(Team lead)/Assignment 2/qn4and5.py
@@ -0,0 +1,15 @@
+QUESTION NO 4:
+
+What are the other frameworks that can be used with python?
+	->Django
+	->Bottle
+	->Pyramid
+	->web2py
+
+
+
+QUESTION NO 5:
+
+Full form of WSGI
+
+	-> Web Server Gateway Interface
